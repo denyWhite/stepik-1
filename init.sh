06bd8daea6c412e0979c10893b83ac697c9769e0
Artyom Bratov <chernitca@ya.ru>
2019-02-10 19:22:03 +0300

    keks

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx stop
-nginx -c /etc/nginx/sites-enabled/test.conf
+sudo nginx -c /etc/nginx/sites-enabled/test.conf
 
 # settings for gunicorn
 sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
-sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
-#sudo gunicorn -c /home/box/web/etc/gunicorn.py
+#sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
+cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
 
 # settings for mysql
 sudo /etc/init.d/mysql start
