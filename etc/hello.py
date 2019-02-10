c787b2bc60a0576c78c614f521e3cbef2db0d0fa
Artyom Bratov <chernitca@ya.ru>
2019-02-10 18:21:38 +0300

    keks

--- etc/hello.py
+++ etc/hello.py
@@ -1,8 +1,2 @@
-CONFIG = {
-  'mode':'wsgi',
-  'working_dir': '/home/box/web/',
-  'args': (
-  '--bind=0.0.0.0:8080',
-  'hello:application'
-  ),
-}
+bind="0.0.0.0:8080"
+pythonpath="/home/box/web/"
